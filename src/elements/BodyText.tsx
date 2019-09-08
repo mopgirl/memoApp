af93081d24ba4d00a7107459218e9e101c95a632
masayuki.katada <masayuki.katada0307@gmail.com>
2019-09-08 13:24:45 +0900

    save

---
+++ src/elements/BodyText.tsx
@@ -0,0 +1,27 @@
+import react from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import * as React from 'react';
+
+class BodyText extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text style={styles.container}>
+                    {this.props.children}
+                </Text>
+            </View>
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#eee',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default BodyText;
