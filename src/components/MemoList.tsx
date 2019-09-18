6b6b1bf61a37185b08beae82ff590a7283ace453
masayuki.katada <masayuki.katada0307@gmail.com>
2019-09-19 02:46:01 +0900

    save

--- src/components/MemoList.tsx
+++ src/components/MemoList.tsx
@@ -5,10 +5,10 @@ class MemoList extends React.Component {
     renderMemo({item}) {
         return (
             <TouchableHighlight onPress={ () => {
-                this.props.navigation.navigate('MemoDetail');
+                this.props.navigation.navigate('MemoDetail', {memo : item});
             } }>
                 <View style={ styles.memoListItem }>
-                    <Text style={ styles.memoTitle }>{ item.body }</Text>
+                    <Text style={ styles.memoTitle }>{ item.body.substr(0,10) }</Text>
                     <Text style={ styles.memoDate }>201918</Text>
                 </View>
             </TouchableHighlight>
--- src/screens/MemoDetailScreen.tsx
+++ src/screens/MemoDetailScreen.tsx
@@ -3,21 +3,36 @@ import { StyleSheet, Text, View } from 'react-native';
 import CircleButton, { ICON_COLOR, ICON_NAME } from '../elements/CircleButton';
 
 class MemoDetailScreen extends React.Component {
+    state = {
+        memo : {},
+    };
+
+    componentWillMount(): void {
+        const {params} = this.props.navigation.state;
+        this.setState({memo : params.memo});
+    }
+
+    dateString(createdOn): string {
+        return createdOn.toDate().toISOString();
+    }
+
     render() {
+        const {memo} = this.state;
         return (
             <View style={ styles.container }>
                 <View style={ styles.memoHeader }>
                     <View>
-                        <Text style={ styles.memoHeaderTitle }>講座のアイディア</Text>
-                        <Text style={ styles.memoHeaderDate }>2019/01/01</Text>
+                        <Text style={ styles.memoHeaderTitle }>{ memo.body.substr(0, 10) }</Text>
+                        <Text style={ styles.memoHeaderDate }>{ this.dateString(memo.createdOn) }</Text>
                     </View>
                 </View>
 
                 <View style={ styles.memoContent }>
-                    <Text>講座のアイディアです</Text>
+                    <Text style={ styles.memoBody }>{ memo.body }</Text>
                 </View>
                 <CircleButton icon={ ICON_NAME.pencil } fontSize={ 30 } color={ ICON_COLOR.pink }
-                              style={ styles.editButton } onPress={ () => { this.props.navigation.navigate('MemoEdit');
+                              style={ styles.editButton } onPress={ () => {
+                    this.props.navigation.navigate('MemoEdit');
                 } }></CircleButton>
             </View>
         );
@@ -25,37 +40,41 @@ class MemoDetailScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
+    container : {
+        flex : 1,
+        width : '100%',
     },
-    memoHeader: {
-        height: 100,
-        backgroundColor: '#17313C',
-        justifyContent: 'center',
-        padding: 10,
+    memoHeader : {
+        height : 100,
+        backgroundColor : '#17313C',
+        justifyContent : 'center',
+        padding : 10,
 
     },
 
-    memoHeaderTitle: {
-        color: '#fff',
-        fontSize: 24,
-        fontWeight: 'bold',
+    memoHeaderTitle : {
+        color : '#fff',
+        fontSize : 24,
+        fontWeight : 'bold',
     },
-    memoHeaderDate: {
-        fontSize: 12,
-        color: '#fff',
+    memoHeaderDate : {
+        fontSize : 12,
+        color : '#fff',
     },
-    memoContent: {
-        paddingTop: 30,
-        paddingLeft: 20,
-        paddingRight: 20,
-        paddingBottom: 20,
-        backgroundColor: '#ddd',
-        flex: 1,
+    memoBody : {
+        lineHeight : 22,
+        fontSize : 15,
     },
-    editButton: {
-        top: 75,
+    memoContent : {
+        paddingTop : 30,
+        paddingLeft : 20,
+        paddingRight : 20,
+        paddingBottom : 20,
+        backgroundColor : '#ddd',
+        flex : 1,
+    },
+    editButton : {
+        top : 75,
 
     },
 });
